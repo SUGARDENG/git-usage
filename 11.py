99e39c83c6d5718bdaae87137b4015d339f9fee5
SUGARDENG <ziyuandeng98@gmail.com>
2019-06-20 20:23:57 +0800

    1.py copy to 2.py

---
+++ 11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#I was changed in dev branch!
+#edited in master and dev
+#github
+#diff head
+print("hello world!")
+print("hello David!")
+print("hello sugardeng!")
+print("hello sir!")
+print("come man! hh")
